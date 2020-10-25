a286698f0e9b27114fed4dbd7aa0a0c8af5e1621
pjmantoss <joelptoss@gmail.com>
2020-10-25 16:16:15 +0100

    update script

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ async function submitPost(e){
 
     let title = document.getElementById("titleInput").value;
     let body = document.getElementById("bodyInput").value
+
+    const options = {
+        method: "POST",
+        body: {title: title, body: body},
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    };
+
+    const postPromise = await fetch("https://jsonplaceholder.typicode.com/posts", options)
 }
